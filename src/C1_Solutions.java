b12c3cc9a3bba389dff0fa577bb4994ab633736e
Stevens <stevensm@formfill.co.uk>
2017-06-16 11:20:51 +0100

    empty c2

--- src/C1_Solutions.java
+++ src/C1_Solutions.java
@@ -6,7 +6,7 @@ import java.io.FileFilter;
 
 public class C1_Solutions {
 
-    //1.1
+    //2.1
     public File[] subDirsWithFilter( File dir){
 
         return dir.listFiles(new FileFilter() {
@@ -17,19 +17,24 @@ public class C1_Solutions {
         });
     }
 
-    //1.2
+    //2.2
     public File[] subDirLambda( File dir){
         return dir.listFiles( f -> f.isDirectory());
     }
 
-    //2
+    //3
     public String[] extensionsInDir (File dir, String ext){
 
         return dir.list( (directory, name) -> name.endsWith(ext));
     }
 
-    //3
-    
+    //4
+    public void lambdaSort(File[] theFiles){
+
+        
+
+    }
+
 
 
 }
---
+++ src/C2_Solutions.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by stevensm on 16/06/2017.
+ */
+public class C2_Solutions {
+}
